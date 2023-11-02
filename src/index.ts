3b076d35d262514330a6cc7276e6892d9d827ccf
gandolphinnn <gandolfiluca03@gmail.com>
2023-11-02 14:46:12 +0100

    Better Monad's conditions

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,42 @@ export interface KeyValuePair<K, V> {
     key: K,
 	value: V;
 }
+type BoolFunction = (val) => boolean
 export class Monad {
 	value: any;
-	history:Array<KeyValuePair<Function, any>>
+	history:Array<KeyValuePair<Function, any>>;
+	condition: BoolFunction;
+	keepDefaultConditions: boolean;
 
-	constructor(value: any, history:Array<KeyValuePair<Function, any>>= []) {
+	constructor(value: any, history:Array<KeyValuePair<Function, any>> = [{key: () => {'INIT'}, value: value}]) {
 		this.value = value;
 		this.history = history;
+		this.SetConditions();
 	}
 
-	apply(func: Function) {
-		if (this.value == null)
-			return this;
+	Apply(func: Function) {
+		let newVal: any;
+		let kvp: KeyValuePair<Function, any>;
+		try {
+			newVal = func(this.value);
+			if (this.condition(newVal)) {
+				throw new Error('Monad custom condition failed');
+			}
+			if (this.keepDefaultConditions && (isNaN(newVal) || newVal === null)) {
+				throw new Error('Monad default condition failed');
+			}
+			kvp = {key: func, value: newVal};
+		} catch (error) {
+			kvp = {key: func, value: error};
+			newVal = this.value;
+		}
+		return new Monad(newVal, [...this.history, kvp]).SetConditions(this.keepDefaultConditions, this.condition);
+	}
 
-		let value = func(this.value);
-		let kvp: KeyValuePair<Function, any> = {key: func, value: value}
-		return new Monad(value, [...this.history, kvp]);
+	SetConditions(keepDefaultConditions: boolean = true, func: BoolFunction = (v: any) => {return false}) {
+		this.keepDefaultConditions = keepDefaultConditions;
+		this.condition = func;
+		return this
 	}
 }
 export function Rand(min: number, max: number) {
--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,10 @@
-import { Monad, Plural } from "./index.js";
+import { Monad } from "./index.js";
+let x = []
 let m = new Monad(12)
-		.apply((v:number) => {return v*2})
-		.apply((v:number) => {return v-4})
-		.apply((v:number) => {return v.toString()})
+		.Apply((v:number) => {return v*2}).SetConditions(true, (v:number) => {return v %2 == 0})
+		.Apply((v:number) => {return v+2}).SetConditions()
+		.Apply((v:number) => {return v/x[1]})
+		.Apply((v:number) => {return v-4})
+		.Apply((v:number) => {return v.toString()})
 
-console.log(m)
-console.log(Plural(2))
+console.table(m)
